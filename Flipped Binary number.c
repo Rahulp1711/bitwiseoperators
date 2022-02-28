e11a5bc5a1283cfb6ce1b12e9fdfc81635f791fe
Rahulp1711 <99257880+Rahulp1711@users.noreply.github.com>
2022-02-28 12:25:46 +0530

    upload flipped binary number code

---
+++ Flipped Binary number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int num,flippedNumber;
+    printf("Enter the any number:");
+    scanf("%d",&num);
+    flippedNumber=~num;
+    printf("Original number=%d(in decimal)\n",num);
+    printf("Number after bits are flipped=%d(in decimal)",flippedNumber);
+    return 0;
+}
